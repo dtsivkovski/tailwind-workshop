038e6bd38bdc03a3af1b94ad039b7155003e98da
Dylan Ravel <dylanm.ravel@gmail.com>
2026-04-04 05:08:15 -0700

    Created project

---
+++ app/(main)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <main className="flex w-[100vw] flex-1">
+        <div></div>
+      </main>
+    </div>
+  );
+}
