bf9fc47669437b2a851341933ee837bbb3cf904f
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2026-04-05 15:17:29 -0700

    fix small typing thing

--- app/exercise/5/page.tsx
+++ app/exercise/5/page.tsx
@@ -7,14 +7,14 @@ export default function ExerciseTwo() {
           <h2>Computer Science Club</h2>
           <p>Chapman University</p>
           <p>Our goal is to help CS students meet new people, build and strengthen their skills, and have fun!</p>
-          <p>Featured Events:</p>
+          <p>Featured Events</p>
           <ul>
             <li>PantherHacks</li>
             <li>Boolean Bonfire</li>
             <li>Presentation Night</li>
           </ul>
           <div>
-            <button className="border border-black">Join now!</button>
+            <button className="border border-black">Join now</button>
           </div>
         </div>
         {/* Don't edit above */}
